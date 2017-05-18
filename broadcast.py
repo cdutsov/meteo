68b4a397f40cbaff8a92cc17bbd3eca39216e8ce
cdutsov <chavcho93@gmail.com>
2017-05-18 17:44:31 +0000

    init comit

---
+++ broadcast.py
@@ -0,0 +1,38 @@
+from Adafruit_BME280 import *
+import paho.mqtt.client as paho
+import veml6070
+
+
+broker="127.0.0.1"
+port=1883
+def on_publish(client,userdata,result):             #create function for callback
+    print("data published \n")
+    pass
+
+def main(stdscr):
+    sensor = BME280(p_mode=BME280_OSAMPLE_8, t_mode=BME280_OSAMPLE_2, h_mode=BME280_OSAMPLE_1, filter=BME280_FILTER_16)
+    tstart = time.time()
+    client1= paho.Client("control1")                           #create client object
+    client1.on_publish = on_publish                          #assign function to callback
+    client1.connect(broker,port)                                 #establish connection
+    veml = veml6070.Veml6070()
+    veml.set_integration_time(veml6070.INTEGRATIONTIME_1T)
+    
+    while (1 == 1) :
+        degrees = sensor.read_temperature()
+        pascals = sensor.read_pressure()
+        hectopascals = pascals / 100
+        humidity = sensor.read_humidity()
+        dew_point = sensor.read_dewpoint()
+        uv_raw = veml.get_uva_light_intensity_raw()
+        uv = veml.get_uva_light_intensity()
+	ret= client1.publish("sensors/temperature", "%0.3f" % (degrees))
+	ret= client1.publish("sensors/pressure", "%0.3f" % (hectopascals))
+	ret= client1.publish("sensors/humidity", "%0.3f" % (humidity))
+	ret= client1.publish("sensors/dewpoint", "%0.3f" % (dew_point))
+	ret= client1.publish("sensors/uv", "%0.3f" % (uv))
+	ret= client1.publish("sensors/uv_raw", "%0.3f" % (uv_raw))
+        time.sleep(1)
+
+main(None)
+
---
+++ read_serial.py
@@ -0,0 +1,21 @@
+import serial
+
+ser = serial.Serial(
+    port='COM5',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
+print("connected to: " + ser.portstr)
+count=1
+
+while True:
+    for line in ser.read():
+        line = ser.readline()
+        
+        print(str(count) + str(': ') + chr(line) )
+        count = count+1
+
+ser.close()
