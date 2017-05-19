f81af9fe9600d01569892256c60c7f51fcedbd7f
cdutsov <chavcho93@gmail.com>
2017-05-19 18:51:31 +0300

    test

--- add_to_files.py
+++ add_to_files.py
@@ -31,15 +31,16 @@ def main():
         dew_point = sensor.read_dewpoint()
         uv_raw = veml.get_uva_light_intensity_raw()
         uv = veml.get_uva_light_intensity()
-        dust_particles = get_dust_particles()
+        dust_particles = (get_dust_particles() - 0.8)*0.227*1000
         ret = client1.publish("sensors/temperature", "%0.3f" % (degrees))
         ret = client1.publish("sensors/pressure", "%0.3f" % (hectopascals))
         ret = client1.publish("sensors/humidity", "%0.3f" % (humidity))
         ret = client1.publish("sensors/dewpoint", "%0.3f" % (dew_point))
         ret = client1.publish("sensors/uv", "%0.3f" % (uv))
         ret = client1.publish("sensors/uv_raw", "%0.3f" % (uv_raw))
-        if dust_particles:
-            ret = client1.publish("sensors/dust_particles", "0.3f" % dust_particles)
+        ret = client1.publish("sensors/dust_particles", "%0.3f" % dust_particles)
+
+
 
         with open("/home/pi/temperature.txt", "aw") as file_t:
             file_t.write("%s" % vreme + " %0.3f" % degrees + "\n")
