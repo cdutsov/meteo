7fda23f128bd7b9183ee49f47bd38c356fc3bd0d
cdutsov <chavcho93@gmail.com>
2017-05-19 19:02:41 +0300

    test

--- add_to_files.py
+++ add_to_files.py
@@ -31,7 +31,7 @@ def main():
         dew_point = sensor.read_dewpoint()
         uv_raw = veml.get_uva_light_intensity_raw()
         uv = veml.get_uva_light_intensity()
-        dust_particles = (get_dust_particles() - 0.8)*0.227*1000
+        dust_particles = (get_dust_particles() - 0.5)*0.227*1000
         ret = client1.publish("sensors/temperature", "%0.3f" % (degrees))
         ret = client1.publish("sensors/pressure", "%0.3f" % (hectopascals))
         ret = client1.publish("sensors/humidity", "%0.3f" % (humidity))
