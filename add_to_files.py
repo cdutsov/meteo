56f52bb3197b819e29766988235e317a48599213
Chavdar <cdutsov@github.com>
2017-05-19 13:09:15 +0300

    sdfgsdgs

--- add_to_files.py
+++ add_to_files.py
@@ -21,10 +21,9 @@ def main():
     veml = veml6070.Veml6070()
     veml.set_integration_time(veml6070.INTEGRATIONTIME_1T)
 
-    file_t = open("temperature.txt", "w")
-    file_p = open("pressure.txt", "w")
-    file_h = open("humidity.txt", "w")
-    file_uv = open("uv.txt", "w")
+    file_p = open("pressure.txt", "aw")
+    file_h = open("humidity.txt", "aw")
+    file_uv = open("uv.txt", "aw")
 
     while True:
         vreme = time.strftime("%H:%M:%S")
@@ -42,11 +41,16 @@ def main():
         ret = client1.publish("sensors/uv", "%0.3f" % (uv))
         ret = client1.publish("sensors/uv_raw", "%0.3f" % (uv_raw))
 
-        file_t.write("%s" % vreme + " %0.3f" % degrees + "\n")
+        with open("temperature.txt", "aw") as file_t:
+            file_t.write("%s" % vreme + " %0.3f" % degrees + "\n")
         file_p.write("%s" % vreme + " %0.3f" % hectopascals + "\n")
         file_h.write("%s" % vreme + " %0.3f" % humidity + "\n")
         file_uv.write("%s" % vreme + " %0.3f" % uv + "\n")
 
+        file_t.close()
+        file_p.close()
+        file_h.close()
+        file_uv.close()
         time.sleep(3)
 
 
