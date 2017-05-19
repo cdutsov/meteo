e8ecaf27c742843f780a2a526e6dc1d765601f5c
Chavdar <cdutsov@github.com>
2017-05-19 12:21:52 +0300

    read ser

--- add_to_files.py
+++ add_to_files.py
@@ -21,7 +21,7 @@ def main():
     veml = veml6070.Veml6070()
     veml.set_integration_time(veml6070.INTEGRATIONTIME_1T)
 
-    file_t = open(“tem”, ”w”)
+    file = open(“tem”, ”w”)
     file_p = open(“pressure.txt”, ”w”)
     file_h = open(“humidity.txt”, ”w”)
     file_uv = open(“uv.txt”, ”w”)
@@ -44,7 +44,7 @@ def main():
         ret = client1.publish("sensors/uv", "%0.3f" % (uv))
         ret = client1.publish("sensors/uv_raw", "%0.3f" % (uv_raw))
 
-        file_t.write(time.strftime("%H:%M:%S"), degrees)
+        file.write(time.strftime("%H:%M:%S"), degrees)
         file_p.write(time.strftime("%H:%M:%S"), hectopascals)
         file_h.write(time.strftime("%H:%M:%S"), humidity)
         file_uv.write(time.strftime("%H:%M:%S"), uv)
