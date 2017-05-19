0b941b60d7838dd6174a30a55478ccde9a68ad74
cdutsov <chavcho93@gmail.com>
2017-05-19 18:50:16 +0300

    test

--- bcast.py
+++ bcast.py
@@ -31,6 +31,7 @@ def main():
         uv_raw = veml.get_uva_light_intensity_raw()
         uv = veml.get_uva_light_intensity()
         dust_particles = (get_dust_particles() - 0.8)*0.227*1000
+        a = 1
 
         ret = client1.publish("sensors/temperature", "%0.3f" % (degrees))
         ret = client1.publish("sensors/pressure", "%0.3f" % (hectopascals))
