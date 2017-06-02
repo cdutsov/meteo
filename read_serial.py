d3874e8cc92e89bbb155993b20770e5c9f88bd71
cdutsov <chavcho93@gmail.com>
2017-06-02 09:58:14 +0300

    median of dust particles

--- read_serial.py
+++ read_serial.py
@@ -1,17 +1,22 @@
 import serial
+import pynmea2
 
 
 def get_dust_particles():
     with serial.Serial('/dev/ttyS0', 9600, timeout=1) as ser:
         sentence = ser.readline()
-        return sentence
         dat = 0.
-        # try:
-        #     dat = float(ser.readline().rsplit(',')[1])
-        # except:
-        #     dat = False
-        # return dat
+        try:
+            dat = float(sentence.rsplit(',')[1])
+        except:
+            dat = False
+        return dat
 
+def get_gps():
+    with serial.Serial('/dev/ttyS0', 9600, timeout=1) as ser:
+        sentence = ser.readline().split('$', 1)
+        if len(sentence) > 1:
+            print pynmea2.parse(sentence[1])
 
 # while True:
 #     with serial.Serial('/dev/ttyS0', 9600, timeout=1) as ser:
