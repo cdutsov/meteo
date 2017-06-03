2cda95332738f0b1b6a05465113e3cd63b6f532d
cdutsov <chavcho93@gmail.com>
2017-06-03 10:18:13 +0300

    median of dust particles

--- add_to_files.py
+++ add_to_files.py
@@ -20,10 +20,10 @@ def on_publish(client, userdata, result):  # create function for callback
 
 
 def publish_data(client, data):
-    # client.publish("sensors/temperature", "%0.3f" % (data["temperature"]), retain=True)
-    # client.publish("sensors/pressure", "%0.3f" % data["pressure"])
-    # client.publish("sensors/humidity", "%0.3f" % data["humidity"])
-    # client.publish("sensors/dewpoint", "%0.3f" % data["dew_point"])
+    client.publish("sensors/temperature", "%0.3f" % (data["temperature"]), retain=True)
+    client.publish("sensors/pressure", "%0.3f" % data["pressure"])
+    client.publish("sensors/humidity", "%0.3f" % data["humidity"])
+    client.publish("sensors/dewpoint", "%0.3f" % data["dew_point"])
     client.publish("sensors/uv", "%0.3f" % data["uv"])
     client.publish("sensors/uv_raw", "%0.3f" % data["uv_raw"])
     client.publish("sensors/dust_particles", "%0.3f" % data["dust_particles"])
@@ -42,7 +42,7 @@ def append_data(data):
 
 
 def main():
-    # sensor = BME280(p_mode=BME280_OSAMPLE_8, t_mode=BME280_OSAMPLE_2, h_mode=BME280_OSAMPLE_1, filter=BME280_FILTER_16)
+    sensor = BME280(p_mode=BME280_OSAMPLE_8, t_mode=BME280_OSAMPLE_2, h_mode=BME280_OSAMPLE_1, filter=BME280_FILTER_16)
     tstart = time.time()
     client1 = paho.Client("control1")  # create client object
     client1.on_publish = on_publish  # assign function to callback
@@ -61,11 +61,11 @@ def main():
     #     publish_data(client=client1, data=data)
     while True:
         data["datetime"] = datetime.datetime.now()
-        # data["temperature"] = sensor.read_temperature()
-        # pascals = sensor.read_pressure()
-        # data["pressure"] = pascals / 100
-        # data["humidity"] = sensor.read_humidity()
-        # data["dew_point"] = sensor.read_dewpoint()
+        data["temperature"] = sensor.read_temperature()
+        pascals = sensor.read_pressure()
+        data["pressure"] = pascals / 100
+        data["humidity"] = sensor.read_humidity()
+        data["dew_point"] = sensor.read_dewpoint()
         data["uv_raw"] = veml.get_uva_light_intensity_raw()
         data["uv"] = veml.get_uva_light_intensity()
         # data["dust_particles"] = round((get_dust_particles() - 0.55) * 0.227 * 1000, 0)
@@ -83,19 +83,19 @@ def main():
         publish_data(client=client1, data=data)
         # append_data(data)
 
-        # with open("/home/pi/temperature.txt", "aw") as file_t:
-        #     file_t.write("%s" % data["datetime"] + " %0.3f" % data["temperature"] + "\n")
-        # with open("/home/pi/pressure.txt", "aw") as file_p:
-        #     file_p.write("%s" % data["datetime"] + " %0.3f" % data["pressure"] + "\n")
-        # with open("/home/pi/humidity.txt", "aw") as file_h:
-        #     file_h.write("%s" % data["datetime"] + " %0.3f" % data["humidity"] + "\n")
-        # with open("/home/pi/uv.txt", "aw") as file_uv:
-        #     file_uv.write("%s" % data["datetime"] + " %0.3f" % data["uv"] + "\n")
-        #
-        # file_t.close()
-        # file_p.close()
-        # file_h.close()
-        # file_uv.close()
+        with open("/home/pi/temperature.txt", "aw") as file_t:
+            file_t.write("%s" % data["datetime"] + " %0.3f" % data["temperature"] + "\n")
+        with open("/home/pi/pressure.txt", "aw") as file_p:
+            file_p.write("%s" % data["datetime"] + " %0.3f" % data["pressure"] + "\n")
+        with open("/home/pi/humidity.txt", "aw") as file_h:
+            file_h.write("%s" % data["datetime"] + " %0.3f" % data["humidity"] + "\n")
+        with open("/home/pi/uv.txt", "aw") as file_uv:
+            file_uv.write("%s" % data["datetime"] + " %0.3f" % data["uv"] + "\n")
+
+        file_t.close()
+        file_p.close()
+        file_h.close()
+        file_uv.close()
         time.sleep(3)
 
 
