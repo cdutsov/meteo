b70002b56e34bc8d48ea5d4cff5c7f9a5ea0d88c
cdutsov <chavcho93@gmail.com>
2017-06-03 10:02:22 +0300

    median of dust particles

--- add_to_files.py
+++ add_to_files.py
@@ -83,19 +83,19 @@ def main():
         publish_data(client=client1, data=data)
         # append_data(data)
 
-        with open("/home/pi/temperature.txt", "aw") as file_t:
-            file_t.write("%s" % data["datetime"] + " %0.3f" % data["temperature"] + "\n")
-        with open("/home/pi/pressure.txt", "aw") as file_p:
-            file_p.write("%s" % data["datetime"] + " %0.3f" % data["pressure"] + "\n")
-        with open("/home/pi/humidity.txt", "aw") as file_h:
-            file_h.write("%s" % data["datetime"] + " %0.3f" % data["humidity"] + "\n")
-        with open("/home/pi/uv.txt", "aw") as file_uv:
-            file_uv.write("%s" % data["datetime"] + " %0.3f" % data["uv"] + "\n")
-
-        file_t.close()
-        file_p.close()
-        file_h.close()
-        file_uv.close()
+        # with open("/home/pi/temperature.txt", "aw") as file_t:
+        #     file_t.write("%s" % data["datetime"] + " %0.3f" % data["temperature"] + "\n")
+        # with open("/home/pi/pressure.txt", "aw") as file_p:
+        #     file_p.write("%s" % data["datetime"] + " %0.3f" % data["pressure"] + "\n")
+        # with open("/home/pi/humidity.txt", "aw") as file_h:
+        #     file_h.write("%s" % data["datetime"] + " %0.3f" % data["humidity"] + "\n")
+        # with open("/home/pi/uv.txt", "aw") as file_uv:
+        #     file_uv.write("%s" % data["datetime"] + " %0.3f" % data["uv"] + "\n")
+        #
+        # file_t.close()
+        # file_p.close()
+        # file_h.close()
+        # file_uv.close()
         time.sleep(3)
 
 
